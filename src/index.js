3ae1a70a7c31ebb384984d03945e04d7400ab243
grubbyhunter <grubbyhunter@gmail.com>
2019-03-19 20:47:50 +0800

    修改项目结构

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./webresource/css/common.css"
-import Index from "./components/index"
-import Header from "./components/common/header"
-import Footer from "./components/common/footer"
-import * as serviceWorker from "./serviceWorker"
-import { BrowserRouter, Route } from "react-router-dom"
+import React from "react";
+import ReactDOM from "react-dom";
+import "./webresource/css/common.css";
+import Index from "./components/index";
+import Header from "./components/common/header";
+import Footer from "./components/common/footer";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <div className="container">
-      <Header />
-      <Route path="/" component={Index} />
-      <Footer />
-    </div>
-  </BrowserRouter>,
+  <div className="container">
+    <Header />
+    <Index />
+    <Footer />
+  </div>,
   document.getElementById("root")
-)
-serviceWorker.unregister()
+);
+serviceWorker.unregister();
