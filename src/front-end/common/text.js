c772bd39d8a79ef4aecb44e2c5ecc8b5afb8d7a7
grubbyhunter <grubbyhunter@gmail.com>
2019-03-23 12:35:08 +0800

    提交text实体文件

---
+++ src/front-end/common/text.js
@@ -0,0 +1,17 @@
+/**
+ * 公共文字内容
+ * @Component text.js
+ * @copyright art
+ * @author  shenbo<grubbyhunter@gmail.com>
+ */
+const text = {
+  MENU:[
+    {title:"首页"},
+    {title:"课程分类"},
+    {title:"特训教室"},
+    {title:"讲师"},
+    {title:"学员作业"}
+  ]
+}
+
+export default text
--- src/front-end/components/common/header.jsx
+++ src/front-end/components/common/header.jsx
@@ -5,8 +5,8 @@
  * @author  shenbo<grubbyhunter@gmail.com>
  */
 import React, { Component } from "react"
-
-class Header extends Component {
+import { MENU} from "../../common/text" 
+class Header extends Component { 
   render() {
     return <header>我是头部</header>
   }
