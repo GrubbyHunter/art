d080e30b2a9e55fa27346821b9697ea198ef950c
grubbyhunter <grubbyhunter@gmail.com>
2019-03-24 23:01:50 +0800

    提交图片文件

--- src/front-end/components/index/item.jsx
+++ src/front-end/components/index/item.jsx
@@ -6,19 +6,21 @@
  */
 import React from "react"
 import { Icon } from "antd"
+import demo001 from "./../../resource/img/demo001.jpg"
+import userHeader from "./../../resource/img/user_header.jpg"
 
 class ItemComponent extends React.Component {
   render() {
     return (
       <li>
         <div className="item-img">
-          <img />
+          <img src={demo001} />
           <div className="lesson-count">24节课</div>
         </div>
         <div className="item-title">
-          <div className="lesson-title" />
+          <div className="lesson-title">日式插画厚涂入门基础课</div>
           <div className="teacher">
-            <img />
+            <img src={userHeader} />
             <div className="teacher-name">主讲人: 林斌</div>
             <div className="play-num">
               <Icon type="eye" />
