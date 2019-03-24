60d62f303b4b0d1e98378723653fc6d230572fe1
grubbyhunter <grubbyhunter@gmail.com>
2019-03-24 19:11:03 +0800

    修改antd的主题配色

--- src/front-end/components/index/index.jsx
+++ src/front-end/components/index/index.jsx
@@ -1,22 +1,11 @@
 import React from "react"
-import logo from "./../../resource/svg/logo.svg"
-
 import "antd/dist/antd.css"
 import "./../../resource/css/common.less"
 import "./../../resource/css/index.less"
 
 class Index extends React.Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
-    )
+    return <div className="main" />
   }
 }
 
