58bc4270899fcf193f44a065551501064fde2663
grubbyhunter <grubbyhunter@gmail.com>
2019-04-13 13:32:52 +0800

    新增页面跳转

--- src/front-end/components/index/item.jsx
+++ src/front-end/components/index/item.jsx
@@ -12,24 +12,28 @@ import userHeader from "./../../resource/img/user_header.jpg"
 class ItemComponent extends React.Component {
   render() {
     return (
-      <li>
-        <div className="item-img">
+      <li onClick={this.handleClick}>
+        <div className='item-img'>
           <img src={demo001} />
-          <div className="lesson-count">24节课</div>
+          <div className='lesson-count'>24节课</div>
         </div>
-        <div className="item-title">
-          <div className="lesson-title">日式插画厚涂入门基础课</div>
-          <div className="teacher">
+        <div className='item-title'>
+          <div className='lesson-title'>日式插画厚涂入门基础课</div>
+          <div className='teacher'>
             <img src={userHeader} />
-            <div className="teacher-name">主讲人: 林斌</div>
-            <div className="play-num">
-              <Icon type="eye" />
+            <div className='teacher-name'>主讲人: 林斌</div>
+            <div className='play-num'>
+              <Icon type='eye' />
               <span>1237</span>
             </div>
           </div>
         </div>
       </li>
     )
+  }
+
+  handleClick() {
+    window.location.href = "/detail"
   }
 }
 
